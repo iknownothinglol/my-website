992325c02ab891c2891fc53135d02ff502100e8e
zeming wang <126911716+iknownothinglol@users.noreply.github.com>
2025-01-07 19:31:58 +0100

    Update script.js

--- earth/script.js
+++ earth/script.js
@@ -1,1 +1,25 @@
+var cube = document.getElementById('cube');
+var isDragging = false;
+var previousX, previousY;
 
+cube.addEventListener('mousedown', function(e) {
+  isDragging = true;
+  previousX = e.clientX;
+  previousY = e.clientY;
+});
+
+document.addEventListener('mousemove', function(e) {
+  if (!isDragging) return;
+
+  var deltaX = e.clientX - previousX;
+  var deltaY = e.clientY - previousY;
+
+  cube.style.transform = 'rotateX(' + (deltaY + 45) + 'deg) rotateY(' + (deltaX + 45) + 'deg)';
+
+  previousX = e.clientX;
+  previousY = e.clientY;
+});
+
+document.addEventListener('mouseup', function() {
+  isDragging = false;
+});
